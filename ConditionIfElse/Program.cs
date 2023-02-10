054bfeba192b166b2b4e77ec4cca6dc6c2fab8a0
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-10 18:11:50 +0300

    IFELSE ToLower tryed

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "aliya")
+{
+    Console.WriteLine("Yay, thats you! Hello there!");
+}
+else
+{
+    Console.Write(username);
+    Console.WriteLine(", ahh, thats you again.");
+}
